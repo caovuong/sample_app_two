040fe23080661a409e851e5186f32dda8f3ffc8c
ThaoNguyen <tranthinguyen1994@gmail.com>
2017-01-10 09:28:39 +0700

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,2 @@
 class User < ApplicationRecord
-  before_save :downcase_email
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-
-  validates :email, presence: true, length: {maximum: 255}, 
-    format: {with: VALID_EMAIL_REGEX}, uniqueness: true,
-    uniqueness: {case_sensitive: false}
-  validates :name, presence: true, length: {maximum: 50}
-  validates :password, presence: true, length: {minimum: 6}  
-  
-  has_secure_password
-  
-  class << self
-    def digest string
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-        BCrypt::Engine.cost
-      BCrypt::Password.create string, cost: cost
-    end
-  end
-
-  private
-  def downcase_email
-    email.downcase!
-  end
 end
