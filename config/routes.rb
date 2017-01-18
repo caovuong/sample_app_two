cc8299ab9d70e0e797ac69cb53a4a849555dc7fd
ThaoNguyen <tranthinguyen1994@gmail.com>
2017-01-18 08:51:30 +0700

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get "password_resets/new"
+  get "password_resets/edit"
   root "static_pages#home"
-
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/signup", to: "users#new"
@@ -8,5 +9,6 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users, only: [:show]
+  resources :users, except: [:new, :create]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
